0210e00bc5c779ee899db4bed7bc1cd7c742c598
Yigit M. AK <ak.yigitmesut@gmail.com>
2024-11-27 16:48:33 +0300

    minor changes on layout

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -2,10 +2,10 @@ import OptionButton from "./OptionButton"
 
 function Home() {
   return (
-    <>
+    <div className="home">
       <OptionButton link='/UserLookup' title={'User Lookup'} />
       <OptionButton link='/CreateUser' title={'Create New User for Vendor'} />
-    </>
+    </div>
   )
 }
 
