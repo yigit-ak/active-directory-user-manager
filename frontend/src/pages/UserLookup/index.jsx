e6976ad1b6ca7dfd7b4ace8c428e948e1d742ee9
Yigit M. AK <ak.yigitmesut@gmail.com>
2024-12-01 18:01:13 +0300

    navigate user details page from lookup interface

--- frontend/src/pages/UserLookup/index.jsx
+++ frontend/src/pages/UserLookup/index.jsx
@@ -1,22 +1,35 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function UserLookup() {
   const [userId, setUserId] = useState('');
+  const navigate = useNavigate();
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // TODO: send form to the server
+    navigate(`/user-lookup/${userId}`)
   };
 
   const handleInputChange = (event) => {
     setUserId(event.target.value);
   };
 
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      handleSubmit(event);
+    }
+  };
+
   return (
     <div className="user-lookup-page">
       <form id="user-id-form" onSubmit={handleSubmit}>
         <label htmlFor="user-id">User Lookup</label>
-        <input type="text" id="user-id" name="user-id" onChange={handleInputChange} placeholder="Enter user Id here" required />
+        <input type="text" id="user-id" name="user-id"
+          onChange={handleInputChange}
+          onKeyDown={handleKeyDown}
+          placeholder="Enter user Id here"
+          required
+        />
         <button type="submit">Search</button>
       </form>
     </div>
