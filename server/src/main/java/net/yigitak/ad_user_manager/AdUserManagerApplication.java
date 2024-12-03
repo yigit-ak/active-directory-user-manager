46ba946390ad954b37bad745613d4cf72aace7c7
Yigit M. AK <ak.yigitmesut@gmail.com>
2024-12-03 13:43:29 +0300

    backend initialisation

---
+++ server/src/main/java/net/yigitak/ad_user_manager/AdUserManagerApplication.java
@@ -0,0 +1,13 @@
+package net.yigitak.ad_user_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdUserManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdUserManagerApplication.class, args);
+	}
+
+}
