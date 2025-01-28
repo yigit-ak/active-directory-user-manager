9c013853adf3d5840d0e1ac79f060d7a1053b20d
Yigit Ak <ak.yigitmesut@gmail.com>
2025-01-28 13:34:17 +0300

    user account control util added

---
+++ server/src/main/java/net/yigitak/ad_user_manager/util/UserAccountControlUtil.java
@@ -0,0 +1,56 @@
+package net.yigitak.ad_user_manager.util;
+
+public class UserAccountControlUtil {
+
+    // userAccountControl flags
+    private static final int NORMAL_ACCOUNT = 0x200; // 512
+    private static final int DONT_EXPIRE_PASSWD = 0x10000; // 65536
+    private static final int ACCOUNTDISABLE = 0x2; // 2
+
+    /**
+     * Enable user and set non-expiring password
+     *
+     * @return userAccountControl value
+     */
+    public static int getEnabledAccountWithExpiringPassword() {
+        return NORMAL_ACCOUNT | DONT_EXPIRE_PASSWD;
+    }
+
+    /**
+     * Enable user (default interactive login)
+     *
+     * @return userAccountControl value
+     */
+    public static int getEnabledAccount() {
+        return NORMAL_ACCOUNT;
+    }
+
+    /**
+     * Disable an existing user while keeping other flags intact
+     * @param userAccountControl The current userAccountControl value
+     * @return New userAccountControl value with ACCOUNTDISABLE flag set
+     */
+    public static int disableAccount(int userAccountControl) {
+        return userAccountControl | ACCOUNTDISABLE;
+    }
+
+    /**
+     * Enable a previously disabled user while keeping other flags intact
+     * @param userAccountControl The current userAccountControl value
+     * @return New userAccountControl value with ACCOUNTDISABLE flag removed
+     */
+    public static int enableUser(int userAccountControl) {
+        return userAccountControl & ~ACCOUNTDISABLE; // Removes the disabled flag
+    }
+
+    /**
+     * Check if an account is enabled
+     *
+     * @param userAccountControl current value
+     * @return true if enabled, false otherwise
+     */
+    public static boolean isAccountEnabled(int userAccountControl) {
+        return (userAccountControl & ACCOUNTDISABLE) == 0;
+    }
+
+}
