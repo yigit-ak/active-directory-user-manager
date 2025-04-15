9f5e87d6c72f715c86e8e520871f5e43fa2ed1ef
Yigit Ak <ak.yigitmesut@gmail.com>
2025-04-15 15:40:58 +0300

    email service and implementation added

--- server/src/main/java/net/yigitak/ad_user_manager/services/EmailService.java
+++ server/src/main/java/net/yigitak/ad_user_manager/services/EmailService.java
@@ -6,20 +6,6 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-@Service
-public class EmailService {
-
-    @Autowired
-    private  JavaMailSender mailSender;
-
-//    @Async
-    public void sendEmail(String recipient, String subject, String body) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(recipient);
-        message.setFrom("no-reply@yigit.local");
-        message.setSubject(subject);
-        message.setText(body);
-
-        mailSender.send(message);
-    }
+public interface EmailService {
+    void sendPassword(String toEmail, String password);
 }
