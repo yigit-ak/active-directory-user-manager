f99ac4edcf09ea03681fc778efab1a8bb76bf9a5
Yigit Ak <ak.yigitmesut@gmail.com>
2025-04-22 12:35:42 +0300

    vendor controller updated

--- server/src/main/java/net/yigitak/ad_user_manager/controllers/VendorController.java
+++ server/src/main/java/net/yigitak/ad_user_manager/controllers/VendorController.java
@@ -1,5 +1,6 @@
 package net.yigitak.ad_user_manager.controllers;
 
+import net.yigitak.ad_user_manager.dto.VendorDto;
 import net.yigitak.ad_user_manager.services.VendorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -7,17 +8,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("api/v1/vendors")
+@RequestMapping("/api/v1/vendors")
 public class VendorController {
 
-    @Autowired
-    private VendorService vendorService;
+    private final VendorService vendorService;
 
-    @GetMapping
-    public ResponseEntity<?> getAllVendors() {
-        var vendors = vendorService.getAll();
-        return ResponseEntity.ok(vendors);
+    @Autowired
+    public VendorController(VendorService vendorService) {
+        this.vendorService = vendorService;
     }
 
+    @GetMapping
+    public ResponseEntity<List<VendorDto>> getAllVendors() {
+        List<VendorDto> vendors = vendorService.getAll();
+        return ResponseEntity.ok(vendors);
+    }
 }
