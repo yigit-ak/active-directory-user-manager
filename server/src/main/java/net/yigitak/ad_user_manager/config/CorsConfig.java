f9cfb3c6027440c8715d9560f992902d13371557
Yigit Ak <ak.yigitmesut@gmail.com>
2025-04-25 11:52:14 +0300

    update on cors config

--- server/src/main/java/net/yigitak/ad_user_manager/config/CorsConfig.java
+++ server/src/main/java/net/yigitak/ad_user_manager/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(frontendOrigin)
-                .allowedMethods("*")
+                .allowedMethods("GET", "POST", "PUT")
                 .allowedHeaders("*");
     }
 }
